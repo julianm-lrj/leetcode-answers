e33e181a4139916449a74cc73e3938b917e79d24
Julian Morley <julianm@lrjsolutions.ca>
2025-03-07 06:46:06 -0500

    both with an d without built-in func

---
+++ ts/69_sqrt.ts
@@ -0,0 +1,18 @@
+const sqrt= (x: number): number => {
+    /*
+    let a = x
+    let c = 0.00001
+  
+    while (true) {  
+        let b = (a + x / a) / 2;  
+        if (Math.abs(b - a) < c) {  
+            break
+        }  
+        a = b
+    }  
+  
+    return Math.floor(a)
+    */
+   return Math.floor(Math.sqrt(x))
+}
+
